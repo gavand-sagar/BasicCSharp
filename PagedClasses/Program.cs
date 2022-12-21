67373925ece739d2acc4bdefd7f9880438176569
gavand-sagar <gavand.sagar.s@gmail.com>
2022-12-21 22:04:54 +0530

    db

--- PagedClasses/Program.cs
+++ PagedClasses/Program.cs
@@ -2,6 +2,7 @@
 using PagedClasses.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,29 +14,44 @@ namespace PagedClasses
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter number : ");
-            int a = 0;
-            try
-            {
-                int c = 56 / a;
-                int input = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Square = " + (input * input));
-            }
-            catch(DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine("Enter Price: ");
+            string input = Console.ReadLine();
+
+
+
+            authenticate("Sagar","123")
+
+
 
 
 
             Console.ReadLine();
+        }
 
-            //Navigator.NavigateTo(0);
-            
+
+        public static bool authenticate(string usename,string password)
+        {
+            string connectionString = "Data Source=desktop-5vqtp5k;Initial Catalog=Northwind;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand($"SELECT * FROM PRODUCTS WHERE UnitPrice<={input}", connection);
+                var reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int productId = reader.GetInt32(0);
+                    string productName = reader.GetString(1);
+                    decimal price = reader.GetDecimal(5);
+
+                    Console.WriteLine($"Id: {productId} \t price: {price}");
+
+                }
+
+
+            }
+
         }
     }
 }
